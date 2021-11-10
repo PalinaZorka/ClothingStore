c04cbc59523b56637cfa54eb72cc20aff2498dd2
Palina <Palina.Zorka@itechart-group.com>
2021-11-10 21:42:55 +0300

    add add and remove to cart items

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    CLEARE_ITEM_FROM_CART: 'CLEARE_ITEM_FROM_CART',
+    REMOVE_ITEM: 'REMOVE_ITEM'
 };
 
 export default CartActionTypes;
