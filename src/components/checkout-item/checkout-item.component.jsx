a00476c4ca95e92d125515d5ddd5442d646289c0
Palina <Palina.Zorka@itechart-group.com>
2021-11-11 16:53:24 +0300

    added addItem and removeitem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,7 +4,7 @@ import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.ac
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const {name, imageUrl, price, quantity} = cartItem;
     return (
         <div className='checkout-item'>
@@ -13,9 +13,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-                <div className='arrow'>&#10094;</div>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
                     <span className='value'>{quantity}</span>
-                <div className='arrow'>&#10095;</div>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
             <span className='price'>{price}</span>
             <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
