94f1b6777806ef2ec3673dd09556adea2b124202
Palina <Palina.Zorka@itechart-group.com>
2021-12-01 20:10:41 +0300

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,31 +6,50 @@ import {
   firestore,
   convertCollectionsSnapshotToMap,
 } from '../../firebase/firebase.utils.js';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.get().then((snapshot) => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({loading: false});
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route 
+          exact path={`${match.path}`} 
+          render={(props) => <CollectionsOverviewWithSpinner 
+            isLoading={loading} {...props} />} 
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => <CollectionPageWithSpinner 
+            isLoading={loading} {...props} />}
+
         />
       </div>
     );
